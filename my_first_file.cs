ed50728fc5e34999adc0021d0a3c060563a490c0
Mujahid792 <muhammadmujahidali2@gmail.com>
2025-02-06 10:51:18 +0500

    product operation

--- my_first_file.cs
+++ my_first_file.cs
@@ -6,5 +6,7 @@ class Program
     {
         int a = 5, b = 3;
         Console.WriteLine("Sum: " + (a + b));
+        Console.WriteLine("Difference: " + (a - b));
+        Console.WriteLine("Product: " + (a * b));
     }
 }
